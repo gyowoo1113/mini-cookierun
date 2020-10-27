3826e22f5eb0376672459619f630f5a1ddb88fc4
gyowoo1113 <gyowoo1113@users.noreply.github.com>
2020-10-27 20:00:54 +0900

    platform

--- 기말 프로젝트/02/main_state.py
+++ 기말 프로젝트/02/main_state.py
@@ -4,12 +4,13 @@ from pico2d import *
 from gobj import *
 from player import Player
 from background import HorzScrollBackground
+from platform import Platform
 
 canvas_width= 1120
 canvas_height = 630
 
 def enter():
-    gfw.world.init(['bg','player'])
+    gfw.world.init(['bg','enemy','platform','item','player'])
     Player.load_all_images()
 
     center = get_canvas_width() // 2, get_canvas_height() // 2
@@ -24,13 +25,29 @@ def enter():
 #    player.bg = bg
     gfw.world.add(gfw.layer.player, player)
 
+#
+    x = 0
+    cw = 2 * get_canvas_width()
+    while x < cw:
+        t = Platform.T_6x7
+        pf = Platform(t, x, 0)
+        gfw.world.add(gfw.layer.platform, pf)
+        x += pf.width
+
 
 def update():
     gfw.world.update()
 
+
+#    dx = -200 * gfw.delta_time
+
+#    for layer in range(gfw.layer.enemy, gfw.layer.item + 1):
+#        for obj in gfw.world.objects_at(layer):
+#            obj.move(dx)
+
 def draw():
     gfw.world.draw()
-    draw_collision_box()
+#    draw_collision_box()
 
 def handle_event(e):
     # prev_dx = boy.dx
