81fb3ba9e68594bfbde511e0f0957a09df282e45
gyowoo1113 <gyowoo1113@users.noreply.github.com>
2020-11-02 16:44:58 +0900

    해야할일 메모

--- 기말 프로젝트/02/starter.py
+++ 기말 프로젝트/02/starter.py
@@ -3,7 +3,15 @@ import main_state
 
 gfw.run(main_state)
 
-# cookie,jelly size 통일
-# 젤리 기능추가
+# jelly size 통일
+# cookie, jelly -> json (깔끔하게 정리하기)
+# 젤리 기능추가:
+# 체력회복: 장애물 구현 후
+# 보너스: 에디터 제작 후
+# 보스: 장애물 구현 후
+# 스피드: 구현미정( 시간 남았을때)
 # 추가한 jelly 충돌체크 박스 수정필요
-# 젤리, 장애물 기본구현 -> 에디터
+# 장애물 기본구현 -> 에디터
+# 에디터 만든 후 main_state 깔끔하게 정리하기
+# ** player -> jump,magnet,biggest time 재는 cnt 변수를 공유하고 있음
+# ** 오류 생기지 않는지 체크 후 수정필요
