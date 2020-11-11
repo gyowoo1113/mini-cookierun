3065bb026cba9749dc920425b4e78c1a9cf2cf90
gyowoo1113 <gyowoo1113@users.noreply.github.com>
2020-11-11 22:51:13 +0900

    cookie size 감소 -> magnet 수정

---
+++ 기말 프로젝트/03/jelly.py
@@ -0,0 +1,126 @@
+import random
+from pico2d import *
+import gfw
+import gobj
+
+JELLY_SIZE = 37
+# 제일 기본젤리만 만든것
+# 이후 자석젤리, 체력, 크기젤리 타입 추가해야함
+
+class Jelly:
+    images = {}
+    TYPES = ['jelly','magnet','biggest','start','boss','bonus','speed',]
+    FPS = 5
+    player = None
+
+    def __init__(self, type, x, y):
+        if len(Jelly.images) == 0:
+            Jelly.load_all_images()
+
+        self.x, self.y = x, y
+        self.rect = 0,0,37,37
+
+        self.type = self.TYPES[type]
+        self.fidx = 0
+        self.time = 0
+        self.char = 'jelly'
+        self.speed = 500
+        self.moving = 10
+        self.jelly_time =0
+        self.images = Jelly.load_images(self.char)
+
+        #self.target= 0
+        #self.delta = 0
+
+    @staticmethod
+    def load_all_images():
+        Jelly.load_images('jelly')
+
+    @staticmethod
+    def load_images(char):
+        if char in Jelly.images:
+            return Jelly.images[char]
+
+        images = {}
+        count = 0
+        file_fmt = '%s/%s/%s (%d).png'
+        for type in Jelly.TYPES:
+            type_images = []
+            n = 0
+            while True:
+                n += 1
+                fn = file_fmt % (gobj.RES_DIR,char, type, n)
+                if os.path.isfile(fn):
+                    type_images.append(gfw.image.load(fn))
+                else:
+                    break
+                count += 1
+            images[type] = type_images
+        Jelly.images[char] = images
+        print('%d images loaded for %s' % (count, char))
+        return images
+
+    def update(self):
+        self.jelly_time += gfw.delta_time
+        self.time += gfw.delta_time
+        if self.player.MAGNET == True:
+            self.move_to_player()
+
+    def draw(self):
+        self.fidx = round(self.time * Jelly.FPS)
+        images = self.images[self.type]
+        image = images[self.fidx % len(images)]
+        #이미지 사이즈 조절 시 수정필요
+        self.w, self.h = image.w,image.h
+
+        image.draw_to_origin(self.x, self.y)
+
+    def move(self, dx):
+        self.x += dx
+        if self.x + JELLY_SIZE < 0:
+            gfw.world.remove(self)
+
+    def get_bb(self):
+        return self.x,self.y,self.x+self.w,self.y+ self.h
+
+    def update_position(self):
+        self.time += gfw.delta_time
+        self.fidx = round(self.time * Jelly.FPS)
+
+        x,y = self.x,self.y
+        dx,dy = self.delta
+        x += dx * self.speed * gfw.delta_time
+        y += dy * self.speed * gfw.delta_time
+
+        # print(self.pos, self.delta, self.target, x, y, dx, dy)
+
+        tx,ty = self.target
+        if dx > 0 and x >= tx or dx < 0 and x <= tx:
+            x = tx
+            done = True
+        if dy > 0 and y >= ty or dy < 0 and y <= ty:
+            y = ty
+            done = True
+
+        self.x,self.y = x,y
+
+    def move_to_player(self):
+        self.set_target(self.player.pos)
+        self.update_position()
+        #collides = gobj.collides_box(self, self.player)
+
+    def set_target(self, target):
+        x,y = self.x,self.y
+        tx,ty = target
+        dx, dy = tx - x, ty - y
+        distance = math.sqrt(dx**2 + dy**2)
+        if distance == 0: return
+
+        self.target = target
+        self.delta = dx / distance, dy / distance
+
+    def check_player(self):
+        if self.player is None:
+            if gfw.world.count_at(gfw.layer.player) > 0:
+                self.player = gfw.world.object(gfw.layer.player, 0)
+
---
+++ 기말 프로젝트/03/player.py
@@ -0,0 +1,218 @@
+from pico2d import *
+import gfw
+import gobj
+import random
+from jelly import Jelly
+
+PLAYER_SIZE = 270
+
+class Player:
+    SLIDE_DURATION = 1.0
+    ACTIONS = ['dead', 'doublejump', 'jump', 'slide','run','falling']
+    GRAVITY = 2500
+    JUMP = 1000
+    images = {}
+    FPS = 10
+    FIDX = 0
+    SIZE = {'cocoa': 1.4,'yogurt': 1.7}
+    BIG = 1
+    MAGNET = False
+    SUPER = False
+    def __init__(self):
+        if len(Player.images) == 0:
+            Player.load_all_images()
+
+        self.pos = 250, get_canvas_height() // 2- 150
+        self.fidx = 0
+        self.time = 0
+        self.char = random.choice(['cocoa','yogurt'])
+        self.images = Player.load_images(self.char)
+        self.action = 'run'
+        self.delta = 0, 0
+
+        self.mag = 1
+        self.mag_speed = 0
+        self.jump_speed = 0
+        self.cookie_time = 0
+
+        self.w,self.h = 0,0
+        self.cnt = 0
+
+        self.score = 0
+        self.size = self.SIZE[self.char]
+        #self.check = -1
+
+    @staticmethod
+    def load_all_images():
+        Player.load_images('cocoa')
+        Player.load_images('yogurt')
+
+    @staticmethod
+    def load_images(char):
+        if char in Player.images:
+            return Player.images[char]
+
+        images = {}
+        count = 0
+        file_fmt = '%s/cookie/%s/%s (%d).png'
+        for action in Player.ACTIONS:
+            action_images = []
+            n = 0
+            while True:
+                n += 1
+                fn = file_fmt % (gobj.RES_DIR, char, action, n)
+                if os.path.isfile(fn):
+                    action_images.append(gfw.image.load(fn))
+                else:
+                    break
+                count += 1
+            images[action] = action_images
+        Player.images[char] = images
+        print('%d images loaded for %s' % (count, char))
+        return images
+
+    def update(self):
+        self.update_mag()
+        self.cookie_time += gfw.delta_time
+        self.time += gfw.delta_time
+        if self.action in ['jump','doublejump', 'falling']:
+            # print('jump speed:', self.jump_speed)
+            self.move((0, self.jump_speed * gfw.delta_time))
+            self.jump_speed -= Player.GRAVITY * self.mag * gfw.delta_time
+
+        _,foot,_,_ = self.get_bb()
+        if foot < 0:
+            self.move((0, get_canvas_height()))
+        platform = self.get_platform(foot)
+        if platform is not None:
+            l,b,r,t = platform.get_bb()
+            if self.action in ['run', 'slide']:
+                if foot > t:
+                    self.action = 'falling'
+                    self.jump_speed = 0
+            else:
+                # print('falling', t, foot)
+                if self.jump_speed < 0 and int(foot) <= t:
+                    self.move((0, t - foot))
+                    self.action = 'run'
+                    self.jump_speed = 0
+                    # print('Now running', t, foot)
+        else:
+            self.move_down_platform()
+
+    def draw(self):
+        self.fidx = round(self.time * Player.FPS)
+        images = self.images[self.action]
+        image = images[self.fidx % len(images)]
+        big = self.size*self.mag
+
+        self.w, self.h = image.w/big, image.h/big
+
+        self.change(len(images))
+
+#        size = PLAYER_SIZE * self.mag, PLAYER_SIZE * self.mag
+        image.draw_to_origin(*self.pos,self.w, self.h)
+
+    def magnify(self):
+        self.mag_speed = 1.0
+    def reduce(self):
+        self.mag_speed = -1.0
+
+    def slide(self):
+        if self.action != 'run': return
+        self.action = 'slide'
+        self.time = 0.0
+
+    def jump(self):
+        if self.action in ['falling','doublejump', 'slide']:
+            return
+        if self.action == 'run':
+            self.action ='jump'
+        elif self.action == 'jump':
+            self.action = 'doublejump'
+            self.cnt = self.fidx
+        self.jump_speed = Player.JUMP #* self.mag
+
+    def change(self,img_len):
+        Player.FIDX = self.fidx - self.cnt
+        if self.action == 'doublejump':
+            if Player.FIDX == img_len:
+                self.action = 'falling'
+        if Player.FIDX > 20.0:
+            if self.mag < 1:
+                self.reduce()
+        elif Player.FIDX > 10.0:
+            if self.MAGNET == True:
+                self.MAGNET = False
+
+    def check(self,item):
+        if item.type == 'jelly':
+            self.score += 150
+        elif item.type == 'biggest':
+            self.cnt = self.fidx
+            if self.mag == 1 and self.mag_speed == 0:
+                self.magnify()
+            self.SUPER = True
+        elif item.type == 'magnet':
+            self.cnt = self.fidx
+            self.MAGNET = True
+
+    def handle_event(self, e):
+        if e.type == SDL_KEYDOWN:
+            if e.key == SDLK_SPACE or e.key == SDLK_UP:
+                self.jump()
+            elif e.key == SDLK_DOWN:
+                self.slide()
+        if e.type == SDL_KEYUP:
+            if e.key == SDLK_DOWN:
+                if self.action == 'slide':
+                    self.cancle()
+
+    def cancle(self):
+         if self.action == 'slide':
+             self.action = 'run'
+
+    def get_bb(self):
+        x,y = self.pos
+        return x, y, x + self.w, y + self.h
+
+    def get_platform(self, foot):
+        selected = None
+        sel_top = 0
+        x,y = self.pos
+        for platform in gfw.world.objects_at(gfw.layer.platform):
+            l,b,r,t = platform.get_bb()
+            if x < l or x > r: continue
+            mid = (b + t) // 2
+            if foot < mid: continue
+            if selected is None:
+                selected = platform
+                sel_top = t
+            else:
+                if t > sel_top:
+                    selected = platform
+                    sel_top = t
+        # if selected is not None:
+        #     print(l,b,r,t, selected)
+        return selected
+
+    def move_down_platform(self):
+        x,y = self.pos
+        y -=  gfw.delta_time
+        self.pos = x,y
+
+    def update_mag(self):
+        if self.mag_speed == 0: return
+
+        self.mag -= self.mag_speed * gfw.delta_time
+        if self.mag < 0.75:
+            self.mag = 0.75
+            self.mag_speed = 0
+        elif self.mag > 1.2:
+            self.mag = 1.0
+            self.mag_speed = 0
+            self.SUPER = False
+
+    def move(self, diff):
+        self.pos = gobj.point_add(self.pos, diff)
+
